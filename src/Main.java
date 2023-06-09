ae5d002e32561c0c8e5b0e4a182f57afcdcdfdec
MrForbesy <mvforbes21@gmail.com>
2023-06-09 11:39:48 -0400

    Updated

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,39 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+//class BirthMonth
+//	main ()
+//		num birthMonth = 0
+//		output “What is your birth month number?”
+//		input birthMonth
+//		if birthMonth >= 1 && <= 12
+//			output “Your birth month is:” + birthMonth
+//		else
+//output “You have entered an incorrect month value:” + birthMonth
+//				endIf
+//			return
+//		end class
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        //Define variables//
+        Scanner in = new Scanner(System.in);
+        int birthMonth;
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        //Ask user to input their birth month number//
+        System.out.printf("What is the number of your birth month?");
+
+        //Stores their number//
+        birthMonth = in.nextInt();
+
+        //Makes sure that the user has entered a correct number and displays the result if correct//
+        if(birthMonth >=1 && birthMonth <= 12)
+        {
+            System.out.printf("Your birth month is " + birthMonth);
+        }
+        else
+        {
+            System.out.printf("You have entered an incorrect birth month value: " + birthMonth);
         }
     }
 }
